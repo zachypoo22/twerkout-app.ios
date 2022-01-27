5d91307efd9c030e09a12cd0a28d0e5b907646f4
Zacheriah Mell <zachm93@live.com>
2022-01-26 21:05:55 -0500

    init

---
+++ twerkout/twerkout/twerkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  twerkoutApp.swift
+//  twerkout
+//
+//  Created by Zach Mell on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct twerkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
